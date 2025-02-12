3972c976a260031aea130bb0f12a5c8f63bea1ad
Esa <esa-pitkanen@hotmail.com>
2025-02-12 19:12:40 +0200

    Get the form data into an array
    The HTML file added the necessary form input fields for now.
    In the JavaScript file, an event was added to the button that captures the data from the form and creates a new book from it in the array.
    In the displayBooks function, the container's child elements of the DOM node had to be removed so that it does not display books multiple times.
    In addition, when adding a book, it is checked that it is unique.

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const container = document.querySelector(".container");
 const addBook = document.querySelector(".add-book");
 const closeModal = document.querySelector(".close");
 const modal = document.querySelector("#modal");
+const submitBtn = document.querySelector("#submit");
+const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector("#book-author");
+const bookYear = document.querySelector("#book-year");
+const allCards = document.querySelectorAll(".card");
 
 // Kirjasto array
 const myLibrary = [];
@@ -16,18 +21,32 @@ function Book(title, author, year) {
 
 // Ota parametrit, Luo kirja ja lisää se array:hyn
 function addBookToLibrary(title, author, year) {
-    const book = new Book(title, author, year);   
-    myLibrary.push(book);
-    console.log(myLibrary);
+    const book = new Book(title, author, year);
+
+    // Add only uniques books
+    let index = -1;
+    for (let i = 0; i < myLibrary.length; i++) {
+        if(myLibrary[i].title === book.title) {
+          index = i;
+        }
+      }
+    
+      if(index > -1) {
+        myLibrary[index] = book;
+      } else {
+        myLibrary.push(book);
+      }
 }
 
 // Manuaalisesti lisätyt kirjat
 addBookToLibrary("The Hobbit", "J.R.R Tolkien", 1937);
 addBookToLibrary("Harry Potter and the Philosopher's Stone", "J. K. Rownling", 1997);
+displayBooks();
 
 // Funktio: Looppaa Arrayn läpi ja näytä kirjat sivulla
 // Näytä taulukossa tai kortissa
 function displayBooks () {
+    container.innerHTML = "";
     myLibrary.forEach(book => {
         const card = document.createElement("div");
         const name = document.createElement("h1");
@@ -46,11 +65,22 @@ function displayBooks () {
     })
 };
 
-displayBooks();
+// displayBooks();
 
 // Painike, jolla käyttäjä voi lisätä uuden kirjan
 addBook.addEventListener("click", () => {
     modal.showModal();
+})
+
+submitBtn.addEventListener("click", (e) => {
+    const newBookTitle =  bookTitle.value;
+    const newBookAuthor = bookAuthor.value;
+    const newBookYear = bookYear.value;
+    e.preventDefault();
+  
+    addBookToLibrary(newBookTitle, newBookAuthor, newBookYear);
+    displayBooks();
+    modal.close();
 })
 
 closeModal.addEventListener("click", () => {
